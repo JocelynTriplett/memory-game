b68fae01c158a6df209a0348a0cb1c9c562f8fe6
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-06 18:43:02 -0400

    pop up 'you lose' when lives run out

--- rework.js
+++ rework.js
@@ -9,7 +9,7 @@
 
 //document.getElementById("startClock").addEventListener("click", startTimer);
 
-let beginningLives = ["O","O","O","O","O","O","O"];
+let beginningLives = ["O","O","O"];
 var lives = [];
 for (var i = 0; i < beginningLives.length; i++) {
   lives.push(beginningLives[i]);
@@ -91,9 +91,14 @@ function pauseBeforeFlip (x,y){
   flippedCards.shift();
   lives.shift();
   document.getElementById("lives").innerHTML = lives.join(' ');
-
+  if (lives.length == 0) {
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  }
+  else {
   for (let i = 0; i < hiddenFaces.length; i++) {
     hiddenCards[i].addEventListener('click', playGame, false);
+    }
     }
 }
 
@@ -155,6 +160,31 @@ for (let i = 0; i < hiddenFaces.length; i++) {
 
 clickCard ();
 
+// Get the modal
+//var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+//btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+//
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
 // function countTimer() {
 //   var timerVar = setInterval(countTimer, 1000);
