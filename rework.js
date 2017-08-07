f79c539fc0f960fced3583323e59ab90ffaadb6e
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-06 21:39:18 -0400

    added tile images

--- rework.js
+++ rework.js
@@ -9,20 +9,29 @@
 
 //document.getElementById("startClock").addEventListener("click", startTimer);
 
-let beginningLives = ["assets/heart.png", "assets/heart.png","assets/heart.png"];
+let beginningLives = ["heart.png", "heart.png","heart.png","heart.png","heart.png",];
 var lives = [];
 var hearts = "";
 for (var i = 0; i < beginningLives.length; i++) {
   lives.push(beginningLives[i]);
 }
 for (var i = 0; i < lives.length; i++) {
-  hearts += `<img src="${lives[i]}" />       `
+  hearts += `<img src="assets/${lives[i]}" />       `
 }
 document.getElementById("lives").innerHTML = hearts;
 
 
 // Card deck values:
-let cards = ["A","B","C","D","E","F","G","H","I","A","B","C","D","E","F","G","H","I"];
+let cards =
+["tile1.png","tile1.png",
+"tile2.png","tile2.png",
+"tile3.png","tile3.png",
+"tile4.png","tile4.png",
+"tile5.png","tile5.png",
+"tile6.png","tile6.png",
+"tile7.png","tile7.png",
+"tile8.png","tile8.png",
+"tile9.png","tile9.png"];
 
 // Shuffle cards:
 function shuffle(array) {
@@ -39,9 +48,12 @@ shuffle(cards);
 // Insert new card values:
 
 for (let i = 0; i < cards.length; i++) {
-  let cardFaceValue = document.createTextNode(cards[i]);
+  let cardFaceImage = document.createElement("img");
+  cardFaceImage.setAttribute("src","assets/"+cards[i]);
   let cardFace = document.getElementById(i+"f");
-  cardFace.appendChild(cardFaceValue);
+  let cardFaceText = document.createTextNode(cards[i]);
+  cardFace.appendChild(cardFaceImage);
+  cardFace.appendChild(cardFaceText);
   cardFace.style.display="none";
 }
 
@@ -98,17 +110,18 @@ function pauseBeforeFlip (x,y){
   lives.shift();
   for (var i = 0; i < lives.length; i++) {
 
-    hearts += `<img src="${lives[i]}" />       `
+    hearts += `<img src="assets/${lives[i]}" />       `
   }
   document.getElementById("lives").innerHTML = hearts;
   if (lives.length == 0) {
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('lose');
     var span = document.getElementsByClassName("close")[0];
     modal.style.display = "block";
     span.onclick = function() {
     modal.style.display = "none";
   }
   }
+
   else {
   for (let i = 0; i < hiddenFaces.length; i++) {
     hiddenCards[i].addEventListener('click', playGame, false);
@@ -140,7 +153,7 @@ function playGame (){
               console.log(face2);
               if (face1[0].textContent != face2[0].textContent) {
 
-                setTimeout(function() { pauseBeforeFlip(face1,face2); },3000);
+                setTimeout(function() { pauseBeforeFlip(face1,face2); },2000);
 
                 //setTimeout(function() { startTimer(parm1); }, startInterval);
 
@@ -154,8 +167,24 @@ function playGame (){
             console.log(face2 + " matched!");
             flippedCards.shift();
             flippedCards.shift();
+            console.log(hiddenFaces.length);
+            console.log(hiddenCards.length);
+            console.log(flippedCards.length);
+            console.log(flippedFaces.length);
+            let matched = document.querySelectorAll('[matched="yes"]')
+            console.log(matched);
+            if (hiddenFaces.length == 0) {
+              var modal = document.getElementById('win');
+              var span = document.getElementsByClassName("close")[0];
+              modal.style.display = "block";
+              span.onclick = function() {
+              modal.style.display = "none";
+            }
+            }
+            else {
             for (let i = 0; i < hiddenFaces.length; i++) {
               hiddenCards[i].addEventListener('click', playGame, false);
+              }
               }
 
             }
