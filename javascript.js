7355b5c9ef4cbf39720f3596f435c0b386edc360
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-06 21:56:17 -0400

    got rid of rework js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,39 @@
 // Shuffle cards function:
 // stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 
-let cards = ["A","B","C","D","E","F","G","H","I","A","B","C","D","E","F","G","H","I"];
+// These load on page refresh:
 
+// Code for timer:
+// https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+
+
+//document.getElementById("startClock").addEventListener("click", startTimer);
+
+let beginningLives = ["heart.png", "heart.png","heart.png","heart.png","heart.png",];
+var lives = [];
+var hearts = "";
+for (var i = 0; i < beginningLives.length; i++) {
+  lives.push(beginningLives[i]);
+}
+for (var i = 0; i < lives.length; i++) {
+  hearts += `<img src="assets/${lives[i]}" />       `
+}
+document.getElementById("lives").innerHTML = hearts;
+
+
+// Card deck values:
+let cards =
+["tile1.png","tile1.png",
+"tile2.png","tile2.png",
+"tile3.png","tile3.png",
+"tile4.png","tile4.png",
+"tile5.png","tile5.png",
+"tile6.png","tile6.png",
+"tile7.png","tile7.png",
+"tile8.png","tile8.png",
+"tile9.png","tile9.png"];
+
+// Shuffle cards:
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -14,10 +45,15 @@ function shuffle(array) {
 
 shuffle(cards);
 
+// Insert new card values:
+
 for (let i = 0; i < cards.length; i++) {
-  let cardFaceValue = document.createTextNode(cards[i]);
+  let cardFaceImage = document.createElement("img");
+  cardFaceImage.setAttribute("src","assets/"+cards[i]);
   let cardFace = document.getElementById(i+"f");
-  cardFace.appendChild(cardFaceValue);
+  let cardFaceText = document.createTextNode(cards[i]);
+  cardFace.appendChild(cardFaceImage);
+  cardFace.appendChild(cardFaceText);
   cardFace.style.display="none";
 }
 
@@ -25,61 +61,242 @@ for (let i = 0; i < cards.length; i++) {
 // www.w3schools.com/howto/howto_js_toggle_hide_show.asp
 
 let allCards = document.querySelectorAll('.card');
-let flippedCards = document.querySelectorAll('.face' && 'style.display[flex]');
 
-console.log(flippedCards);
+// QuerySelectorAll match on attribute value help here: https://stackoverflow.com/questions/10777684/how-to-use-queryselectorall-only-for-elements-that-have-a-specific-attribute-set
+var flippedFaces = document.querySelectorAll('.face' && '[style="display: flex;"]');
+var hiddenFaces = document.querySelectorAll('.face' && '[style= "display: none;"]');
 
-var test = document.querySelectorAll('input[value][type="checkbox"]:not([value=""])');
+var hiddenCards = [];
+for (var i = 0; i < hiddenFaces.length; i++) {
+  hiddenCards.push(hiddenFaces[i].parentElement);
+}
 
-function showFace() {
+var flippedCards = [];
+for (var i = 0; i < flippedFaces.length; i++) {
+  flippedCards.push(flippedFaces[i].parentElement);
+}
+
+hiddenCards[i].addEventListener('click', setTimerVar, false);
+
+function setTimerVar () {
+  var timerVar = setInterval(countTimer, 1000);
+}
+
+var totalSeconds = 0;
+function countTimer() {
+   ++totalSeconds;
+   var hour = Math.floor(totalSeconds /3600);
+   var minute = Math.floor((totalSeconds - hour*3600)/60);
+   var formattedMinutes = ("0" + minute).slice(-2);
+   var seconds = totalSeconds - (hour*3600 + minute*60);
+   var formattedSeconds = ("0" + seconds).slice(-2);
+
+   document.getElementById("timer").innerHTML = "Time: " + formattedMinutes + ":" + formattedSeconds;
+}
+// console.log("hiddenCards: " + hiddenCards);
+// console.log("flippedFaces: " + flippedFaces);
+// console.log("hiddenFaces: " + hiddenFaces);
+// console.log("hiddenCards: " + hiddenFaces.length);
+
+//console.log(flippedCards);
+
+function pauseBeforeFlip (x,y){
+  console.log(x[0]);
+  hearts = "";
+  x[0].style.display = "none";
+  y[0].style.display = "none";
+  flippedCards.shift();
+  flippedCards.shift();
+  lives.shift();
+  for (var i = 0; i < lives.length; i++) {
+
+    hearts += `<img src="assets/${lives[i]}" />       `
+  }
+  document.getElementById("lives").innerHTML = hearts;
+  if (lives.length == 0) {
+    var modal = document.getElementById('lose');
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+    span.onclick = function() {
+    modal.style.display = "none";
+  }
+  }
+
+  else {
+  for (let i = 0; i < hiddenFaces.length; i++) {
+    hiddenCards[i].addEventListener('click', playGame, false);
+    }
+    }
+}
+
+function playGame (){
+  if (flippedCards.length < 2) {
     let face = this.getElementsByClassName("face");
-    // console.log("this",this);
-    // console.log("face[0]",face[0]);
-    // console.log("face[0].style.display",face[0].style.display);
-    if (face[0].style.display === 'none') {
-        face[0].style.display = 'flex';
-    } else {
-        face[0].style.display = 'none';
-    }
-    flippedCards.push(this);
-
-    if (flippedCards.length > 1) {
-      for (let i = 0; i < allCards.length; i++) {
-        allCards[i].removeEventListener('click', showFace, false);
-
+        if (face[0].style.display === 'none') {
+            face[0].style.display = 'flex';
         }
-      setTimeout(matchCards,2000);
-      for (let i = 0; i < allCards.length; i++) {
-        allCards[i].addEventListener('click', showFace, false);
+        flippedCards.push(this);
 
-        }
-    }
+}
 
-    function matchCards(){
+        // console.log(flippedCards);
+        if (flippedCards.length > 1) {
+          for (let i = 0; i < allCards.length; i++) {
+                  allCards[i].removeEventListener('click', playGame, false);
 
-      let face1 = flippedCards[0].getElementsByClassName("face");
-      let face2 = flippedCards[1].getElementsByClassName("face");
-      console.log(face1);
-      console.log(face2);
-      if (face1[0].textContent != face2[0].textContent) {
-      face1[0].style.display = "none";
-      face2[0].style.display = "none";
-      flippedCards.shift();
-      flippedCards.shift();
-    }
+                  }
 
-    else {
-    face1[0].matched = "yes";
-    face2[0].matched = "yes";
-    console.log(face1 + " matched!");
-    console.log(face2 + " matched!");
-    flippedCards.shift();
-    flippedCards.shift();
-    }
+
+              let face1 = flippedCards[0].getElementsByClassName("face");
+              let face2 = flippedCards[1].getElementsByClassName("face");
+              console.log(face1);
+              console.log(face2);
+              if (face1[0].textContent != face2[0].textContent) {
+
+                setTimeout(function() { pauseBeforeFlip(face1,face2); },2000);
+
+                //setTimeout(function() { startTimer(parm1); }, startInterval);
+
+
+            }
+
+            else {
+            face1[0].matched = "yes";
+            face2[0].matched = "yes";
+            console.log(face1 + " matched!");
+            console.log(face2 + " matched!");
+            flippedCards.shift();
+            flippedCards.shift();
+            console.log(hiddenFaces.length);
+            console.log(hiddenCards.length);
+            console.log(flippedCards.length);
+            console.log(flippedFaces.length);
+            let matched = document.querySelectorAll('[matched="yes"]')
+            console.log(matched);
+            if (hiddenFaces.length == 0) {
+              var modal = document.getElementById('win');
+              var span = document.getElementsByClassName("close")[0];
+              modal.style.display = "block";
+              span.onclick = function() {
+              modal.style.display = "none";
+            }
+            }
+            else {
+            for (let i = 0; i < hiddenFaces.length; i++) {
+              hiddenCards[i].addEventListener('click', playGame, false);
+              }
+              }
+
+            }
+          }
+
+  }
+
+
+function clickCard () {
+for (let i = 0; i < hiddenFaces.length; i++) {
+
+  hiddenCards[i].addEventListener('click', playGame, false);
   }
 }
 
-  for (let i = 0; i < allCards.length; i++) {
-    allCards[i].addEventListener('click', showFace, false);
 
-    }
+clickCard ();
+
+// Get the modal
+//var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+//btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+//
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// function countTimer() {
+//   var timerVar = setInterval(countTimer, 1000);
+//   var totalSeconds = 0;
+//   ++totalSeconds;
+//    var hour = Math.floor(totalSeconds /3600);
+//    var minute = Math.floor((totalSeconds - hour*3600)/60);
+//    var seconds = totalSeconds - (hour*3600 + minute*60);
+//
+//    document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+// }
+
+// console.log(flippedCards);
+
+//   for (let i = 0; i < hiddenCards.length; i++) {
+//   hiddenCards[i].addEventListener('onClick', flipCard, false);
+// }
+
+
+//
+// function showFace() {
+//     let face = this.getElementsByClassName("face");
+//     // console.log("this",this);
+//     // console.log("face[0]",face[0]);
+//     // console.log("face[0].style.display",face[0].style.display);
+//     if (face[0].style.display === 'none') {
+//         face[0].style.display = 'flex';
+//     } else {
+//         face[0].style.display = 'none';
+//     }
+//     flippedCards.push(this);
+//
+//     if (flippedCards.length > 1) {
+//       for (let i = 0; i < allCards.length; i++) {
+//         allCards[i].removeEventListener('click', showFace, false);
+//
+//         }
+//       setTimeout(matchCards,2000);
+//       for (let i = 0; i < allCards.length; i++) {
+//         allCards[i].addEventListener('click', showFace, false);
+//
+//         }
+//     }
+//
+//     function matchCards(){
+//
+//       let face1 = flippedCards[0].getElementsByClassName("face");
+//       let face2 = flippedCards[1].getElementsByClassName("face");
+//       console.log(face1);
+//       console.log(face2);
+//       if (face1[0].textContent != face2[0].textContent) {
+//       face1[0].style.display = "none";
+//       face2[0].style.display = "none";
+//       flippedCards.shift();
+//       flippedCards.shift();
+//     }
+//
+//     else {
+//     face1[0].matched = "yes";
+//     face2[0].matched = "yes";
+//     console.log(face1 + " matched!");
+//     console.log(face2 + " matched!");
+//     flippedCards.shift();
+//     flippedCards.shift();
+//     }
+//   }
+// }
+//
+  // for (let i = 0; i < allCards.length; i++) {
+  //   allCards[i].addEventListener('click', showFace, false);
+  //
+  //   }
