fc43acb12b2fc3358484dcccec1497b218eacbc4
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2018-01-26 17:22:30 -0500

    deal image to each card

--- javascript.js
+++ javascript.js
@@ -1,6 +1,37 @@
 let startButton = document.getElementById('startButton');
-
 startButton.addEventListener('click', startGame, false);
+
+let cards =
+["tile1.png","tile1.png",
+"tile2.png","tile2.png",
+"tile3.png","tile3.png",
+"tile4.png","tile4.png",
+"tile5.png","tile5.png",
+"tile6.png","tile6.png",
+"tile7.png","tile7.png",
+"tile8.png","tile8.png",
+"tile9.png","tile9.png"];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
+shuffle(cards);
+
+function deal(array){
+  let faces = document.querySelectorAll( ".face" );
+  for (let i = 0; i < array.length; i++) {
+    let faceImage = document.createElement("img");
+    faceImage.setAttribute("src","assets/"+cards[i]);
+    faces[i].appendChild(faceImage);
+  }
+}
+
 
 function createGameBoard(){
   let gameBoard = document.getElementById("gameBoard");
@@ -8,15 +39,18 @@ function createGameBoard(){
     let row = document.createElement("div");
     row.classList.add('row');
     gameBoard.appendChild(row);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 6; j++) {
       let card = document.createElement("div");
-      card.classList.add('card');
+      card.classList.add('card');let face = document.createElement("div");
+      face.classList.add('face');
       let cardText = document.createTextNode("this is a card!");
       row.appendChild(card);
+      card.appendChild(face);
       card.appendChild(cardText);
       console.log("created element")
     }
   }
+  deal(cards);
 }
 
 function startGame() {
